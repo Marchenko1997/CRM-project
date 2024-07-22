e654984f4a0e66505e13684c7909ae74d82ee352
DrunkCherry1997 <poisonivy1997@ukr.net>
2024-07-22 14:24:42 +0200

    First lessson right commite

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="en">
       <body>{children}</body>
